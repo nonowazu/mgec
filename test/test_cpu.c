1e87f82061f3db5c0f92717c9cb61d73e8ee5e1f
Oowazu Nonowazu <oowazu.nonowazu@gmail.com>
2024-07-13 17:07:00 -0500

    Add basic tests for cpu

--- test/test_cpu.c
+++ test/test_cpu.c
@@ -1,10 +1,24 @@
 #include "cpu.h"
 #include "unity.h"
+#include "unity_internals.h"
 
 void setUp(void){};
 void tearDown(void){};
 
+void test_CPU_should_init() {
+  cpu *c = mgec_new_cpu();
+  TEST_ASSERT_EQUAL_INT16(c->pc, 0);
+  TEST_ASSERT_EQUAL_INT8(c->ac, 0);
+  TEST_ASSERT_EQUAL_INT8(c->x, 0);
+  TEST_ASSERT_EQUAL_INT8(c->y, 0);
+  TEST_ASSERT_EQUAL_INT8(c->sr, 0);
+  TEST_ASSERT_EQUAL_INT8(c->sp, 0);
+}
+
 int main() {
   UNITY_BEGIN();
+
+  RUN_TEST(test_CPU_should_init);
+
   return UNITY_END();
 }
